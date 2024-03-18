4d88eb4d2498c2ee83312dec59c1807723e36e15
ArxhentBreznica <37910236+ArxhentBreznica@users.noreply.github.com>
2024-03-18 13:30:48 +0100

    Update Install-Office.ps1

--- Install-Office.ps1
+++ Install-Office.ps1
@@ -1,3 +1,4 @@
+New-Item -Path C:\ -Name bdotmp -ItemType Directory -ErrorAction SilentlyContinue
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ArxhentBreznica/OSDeployDEV/main/Office.zip" -OutFile C:\bdotmp\Office.zip
 Expand-Archive -Path C:\bdotmp\Office.zip -DestinationPath C:\bdotmp\Office -Force
 Start-Process -FilePath "C:\bdotmp\Office\Deploy-Application.exe"

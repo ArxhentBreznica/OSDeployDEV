603acd4b9306ec717725654c7108f1e45597ca2a
ArxhentBreznica <37910236+ArxhentBreznica@users.noreply.github.com>
2024-12-12 10:56:40 +0100

    Update Install-Languages.ps1

--- Install-Languages.ps1
+++ Install-Languages.ps1
@@ -1,4 +1,5 @@
 Start-Transcript -Path  C:\Temp\InstallLanguage.txt
-Install-Language -Language de-de -CopyToSettings
-Copy-UserInternationalSettingsToSystem
+Install-Language -Language de-de
+Install-Language -Language fr-fr
+Install-Language -Language it-it
 Stop-Transcript

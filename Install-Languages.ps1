5b792dc5103fb949a0ff215da4ecfe5dee943c25
ArxhentBreznica <37910236+ArxhentBreznica@users.noreply.github.com>
2025-02-04 13:15:44 +0100

    Update Install-Languages.ps1

--- Install-Languages.ps1
+++ Install-Languages.ps1
@@ -1,4 +1,4 @@
-#Start-Transcript -Path  C:\Temp\InstallLanguage.txt
-#Install-Language -Language de-de
-#Install-Language -Language fr-fr
-#Stop-Transcript
+Start-Transcript -Path  C:\Temp\InstallLanguage.txt
+Install-Language -Language de-de
+Install-Language -Language fr-fr
+Stop-Transcript
